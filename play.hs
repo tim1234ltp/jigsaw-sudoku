c3577537253cf46ed8f8b3a74169e321a3a44e03
Anton Eriksson <eriksson.e.anton@gmail.com>
2019-12-10 18:56:52 +0800

    Check sudoku constraints after each move

---
+++ play.hs
@@ -0,0 +1,35 @@
+module Play
+( setSudokuState
+, isMoveValid
+) where
+
+import Utils
+
+setSudokuState :: Int -> Int -> Char -> [[(Int, Int, Char, Char)]] -> [[(Int, Int, Char, Char)]] 
+setSudokuState x y newValue positions = map (\row -> setStateInRow x y newValue row) positions
+
+setStateInRow :: Int -> Int -> Char -> [(Int, Int, Char, Char)] -> [(Int, Int, Char, Char)]
+setStateInRow x y newValue row = map (\position -> returnState x y newValue position) row
+
+returnState :: Int -> Int -> Char -> (Int, Int, Char, Char) -> (Int, Int, Char, Char)
+returnState x y newValue position
+  | x == getPosX position && y == getPosY position = (x, y, getPosZone position, newValue)
+  | otherwise = position
+
+isMoveValid :: Int -> Int -> Char -> [[(Int, Int, Char, Char)]] -> Bool
+isMoveValid x y val positions = (isColumnValid val column) && (isRowValid val row) && (isZoneValid val zone)
+  where row = positions !! y
+        zone = concat $ map (\row ->  (filterZoneMembers row (getZone x y positions)) ) positions
+        column = map (\row -> row !! x) positions
+
+filterZoneMembers :: [(Int, Int, Char, Char)] -> Char -> [(Int, Int, Char, Char)]
+filterZoneMembers row targetZone  = filter (\(x, y, zone, val) -> zone == targetZone) row
+
+isRowValid :: Char -> [(Int, Int, Char, Char)] -> Bool
+isRowValid val row =  (length (filter (\(x, y, zone, value) -> value == val) row)) < 2
+
+isColumnValid :: Char -> [(Int, Int, Char, Char)] -> Bool
+isColumnValid val column =  (length (filter (\(x, y, zone, value) -> value == val) column)) < 2
+
+isZoneValid :: Char -> [(Int, Int, Char, Char)] -> Bool
+isZoneValid val zone =  (length (filter (\(x, y, zone, value) -> value == val) zone)) < 2
--- sudoku.hs
+++ sudoku.hs
@@ -1,19 +1,21 @@
 import System.IO
 import Control.Monad
 import Data.Char
+import Data.List
 import Load
 import Save
 import Utils
+import Play
 
 main = forever $ do
-    putStrLn "To start playing, load a game"
+    putStrLn "To start playing, use 'start PATH', where PATH points to a saved game."
     command <- getLine
-    executeCommand command
+    executeStartCommand command
 
-executeCommand :: String -> IO ()
-executeCommand cmd
+executeStartCommand :: String -> IO ()
+executeStartCommand cmd
   | cmd == "start" = startSudokuGame cmd
-  | otherwise = print "Wrong command."
+  | otherwise = putStrLn "Wrong command"
 
 startSudokuGame :: String -> IO ()
 startSudokuGame cmd = do
@@ -24,30 +26,26 @@ startSudokuGame cmd = do
       positions = getSudokuState mapRows originalStateRows
       sudokuMap = concat $ getDrawnSudokuMap positions
   putStrLn (unlines sudokuMap)
-  makeMove positions
+  makeMove positions mapRows
 
-makeMove :: [[(Int, Int, Char, Char)]] -> IO ()
-makeMove positions = forever $ do
+makeMove :: [[(Int, Int, Char, Char)]] -> [String] -> IO ()
+makeMove positions mapRows = do
   putStrLn "Enter the next move in form x y value, e.g. 0 0 2"
   putStrLn "To save the game in its current state use 'save PATH'"
   command <- getLine
-  let args = words command
-      x = digitToInt $ (args !! 0) !! 0
-      y = digitToInt $ (args !! 1) !! 0
-      newValue = (args !! 2) !! 0
-      newPositions = setSudokuState x y newValue positions
-      newSudokuMap = concat $ getDrawnSudokuMap newPositions
-  putStrLn (unlines newSudokuMap)
-  makeMove newPositions
-
-
-setSudokuState :: Int -> Int -> Char -> [[(Int, Int, Char, Char)]] -> [[(Int, Int, Char, Char)]] 
-setSudokuState x y newValue positions = map (\row -> setStateInRow x y newValue row) positions
-
-setStateInRow :: Int -> Int -> Char -> [(Int, Int, Char, Char)] -> [(Int, Int, Char, Char)]
-setStateInRow x y newValue row = map (\position -> returnState x y newValue position) row
-
-returnState :: Int -> Int -> Char -> (Int, Int, Char, Char) -> (Int, Int, Char, Char)
-returnState x y newValue position
-  | x == getPosX position && y == getPosY position = (x, y, getPosZone position, newValue)
-  | otherwise = position
+  if isInfixOf "save" command
+    then saveMapAndState ((words command) !! 1) mapRows positions
+    else do
+      let args = words command
+          x = digitToInt $ (args !! 0) !! 0
+          y = digitToInt $ (args !! 1) !! 0
+          newValue = (args !! 2) !! 0
+          newPositions = setSudokuState x y newValue positions
+          newSudokuMap = concat $ getDrawnSudokuMap newPositions
+      if isMoveValid x y newValue newPositions
+        then do
+          putStrLn (unlines newSudokuMap)
+          makeMove newPositions mapRows
+        else do
+          putStrLn "Move was NOT successful, check that the new entry is unique in its column, zone and row."
+          makeMove positions mapRows
